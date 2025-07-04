0c52691d86c4c1860376a479dd9de7f378f56422
abdul rahman sholeh <focalornet11@gmail.com>
2025-07-04 21:16:17 +0700

    Membuat halaman login untuk user

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::redirect('home', 'dashboard');
+
+Route::get('/auth', [AuthController::class, 'index'])->name('login')->middleware('guest');
