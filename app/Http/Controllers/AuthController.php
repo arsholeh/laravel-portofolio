fe315f3ce520d74050e6cf6a5b95bf02b0379fd1
abdul rahman sholeh <focalornet11@gmail.com>
2025-07-06 14:51:59 +0700

    Menambahkan halaman dashboard admin

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
 use Laravel\Socialite\Facades\Socialite;
 
 class AuthController extends Controller
@@ -23,21 +24,29 @@ class AuthController extends Controller
     public function callback()
     {
 
-
         $user = Socialite::driver('google')->user();
 
         $id = $user->id;
         $email = $user->email;
         $name = $user->name;
+        $avatar = $user->avatar;
+
 
         $cek = User::where('email', $email)->count();
 
         if ($cek > 0) {
+            $avatarFile = $id . '.jpg';
+            $fileContent = file_get_contents($avatar);
+            File::put(public_path("assets/img/user/$avatarFile"), $fileContent);
+
+
             $user = User::updateOrCreate(
+
                 [
                     'email' => $email,
                     'name' => $name,
-                    'google_id' => $id
+                    'google_id' => $id,
+                    'avatar' => $avatarFile
                 ]
             );
 
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 
 
 Route::get('/', function () {
@@ -15,3 +16,5 @@ Route::get('/auth', [AuthController::class, 'index'])->name('login')->middleware
 Route::get('/auth/redirect', [AuthController::class, 'redirect'])->middleware('guest');
 Route::get('/auth/callback', [AuthController::class, 'callback'])->middleware('guest');
 Route::get('/auth/logout', [AuthController::class, 'logout']);
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
