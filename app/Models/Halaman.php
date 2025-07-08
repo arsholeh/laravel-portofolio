2e1e511d4cc6c8f2d3ed04fcec06d8fb26a6f2e1
abdul rahman sholeh <focalornet11@gmail.com>
2025-07-08 11:55:29 +0700

    Membuat model dan migration untuk halaman

---
+++ app/Models/Halaman.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Halaman extends Model
+{
+    protected $table = "halaman";
+    protected $fillable = [
+        'judul',
+        'isi'
+    ];
+}
