889177b191167b95df23fbcf14954ed46fccf23a
abdul rahman sholeh <focalornet11@gmail.com>
2025-07-08 11:57:40 +0700

    Menambahkan semua route halaman dengan prefix

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
+use App\Models\Halaman;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HalamanController;
 use App\Http\Controllers\DashboardController;
-
 
 Route::get('/', function () {
     return view('welcome');
@@ -17,4 +18,13 @@ Route::get('/auth/redirect', [AuthController::class, 'redirect'])->middleware('g
 Route::get('/auth/callback', [AuthController::class, 'callback'])->middleware('guest');
 Route::get('/auth/logout', [AuthController::class, 'logout']);
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
+
+
+
+
+Route::prefix('dashboard')->group(
+    function () {
+        Route::get('/', [DashboardController::class, 'index'])->middleware('auth');
+        Route::resource('halaman', HalamanController::class)->middleware('auth');
+    }
+);
