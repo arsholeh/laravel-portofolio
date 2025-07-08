c0823a7b663bc8eefe8fa620c70332ee0a7f9603
abdul rahman sholeh <focalornet11@gmail.com>
2025-07-08 12:01:16 +0700

    Menambahkan fitur CRUD

--- app/Http/Controllers/HalamanController.php
+++ app/Http/Controllers/HalamanController.php
@@ -24,4 +24,77 @@ class HalamanController extends Controller
     {
         return view('dashboard.halaman.create');
     }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+
+        Session::flash('judul', $request->judul);
+        Session::flash('isi', $request->isi);
+
+        $request->validate([
+            'judul' => 'required',
+            'isi' => 'required',
+
+        ], [
+            'judul.required' => 'Judul wajib diisi',
+            'isi.required' => 'Tulisan wajib diisi'
+        ]);
+
+        $data = [
+            'judul' => $request->judul,
+            'isi' => $request->isi
+        ];
+
+        Halaman::create($data);
+
+        return redirect()->route('halaman.index')->with('success', 'Berhasil menambahkan data');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $halaman = Halaman::where('id', $id)->first();
+        return view('dashboard.halaman.edit', compact('halaman'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            'judul' => 'required',
+            'isi' => 'required',
+
+        ], [
+            'judul.required' => 'Judul wajib diisi',
+            'isi.required' => 'Tulisan wajib diisi'
+        ]);
+
+        $data = [
+            'judul' => $request->judul,
+            'isi' => $request->isi
+        ];
+
+        $halaman = Halaman::findOrFail($id);
+        $halaman->update($data);
+
+        return redirect()->route('halaman.index')->with('success', 'Berhasil mengubah data');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $halaman = Halaman::findOrFail($id);
+        $halaman->delete();
+
+        return redirect()->route('halaman.index')->with('success', 'Berhasil menghapus datas data');
+    }
 }
