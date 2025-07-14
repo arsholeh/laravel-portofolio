493755b0b35830d3f1fc80ae974a596f90fbbae3
abdul rahman sholeh <focalornet11@gmail.com>
2025-07-14 10:01:32 +0700

    Membuat  fitur CRUD untuk halaman experience

---
+++ app/Http/Controllers/ExperienceController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Riwayat;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+
+class ExperienceController extends Controller
+{
+
+    protected $_tipe;
+    function __construct()
+    {
+        $this->_tipe = 'experience';
+    }
+
+
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data = Riwayat::where('tipe', $this->_tipe)->orderBy('tgl_akhir', 'desc')->get();
+        return view('dashboard.experience.index', compact('data'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('dashboard.experience.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        Session::flash('judul', $request->judul);
+        Session::flash('info1', $request->info1);
+        Session::flash('tgl_mulai', $request->tgl_mulai);
+        Session::flash('tgl_akhir', $request->tgl_akhir);
+        Session::flash('isi', $request->isi);
+
+        $request->validate([
+            'judul' => 'required',
+            'info1' => 'required',
+            'tgl_mulai' => 'required',
+            'isi' => 'required'
+
+        ], [
+            'judul.required' => 'Judul wajib diisi',
+            'info1' => 'Nama Perusahaan wajib diisi',
+            'tgl_mulai.required' => 'Tanggal Mulai wajib diisi',
+            'isi.required' => 'Tulisan wajib diisi'
+        ]);
+
+        $data = [
+            'judul' => $request->judul,
+            'info1' => $request->info1,
+            'tipe' => $this->_tipe,
+            'tgl_akhir' => $request->tgl_akhir,
+            'tgl_mulai' => $request->tgl_mulai,
+            'isi' => $request->isi
+        ];
+
+        Riwayat::create($data);
+
+        return redirect()->route('experience.index')->with('success', 'Berhasil menambahkan data experience');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $data = Riwayat::where('id', $id)->where('tipe', $this->_tipe)->first();
+
+        return view('dashboard.experience.edit', compact('data'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        Session::flash('judul', $request->judul);
+        Session::flash('info1', $request->info1);
+        Session::flash('tgl_mulai', $request->tgl_mulai);
+        Session::flash('tgl_akhir', $request->tgl_akhir);
+        Session::flash('isi', $request->isi);
+
+        $request->validate([
+            'judul' => 'required',
+            'info1' => 'required',
+            'tgl_mulai' => 'required',
+            'isi' => 'required'
+
+        ], [
+            'judul.required' => 'Judul wajib diisi',
+            'info1' => 'Nama Perusahaan wajib diisi',
+            'tgl_mulai.required' => 'Tanggal Mulai wajib diisi',
+            'isi.required' => 'Tulisan wajib diisi'
+        ]);
+
+        $data = [
+            'judul' => $request->judul,
+            'info1' => $request->info1,
+            'tipe' => $this->_tipe,
+            'tgl_akhir' => $request->tgl_akhir,
+            'tgl_mulai' => $request->tgl_mulai,
+            'isi' => $request->isi
+        ];
+
+        Riwayat::where('id', $id)->where('tipe', $this->_tipe)->update($data);
+
+        return redirect()->route('experience.index')->with('success', 'Berhasil melakukan update data experience');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        Riwayat::where('id', $id)->where('tipe', $this->_tipe)->delete();
+
+        return redirect()->route('halaman.index')->with('success', 'Berhasil menghapus data experience');
+    }
+}
